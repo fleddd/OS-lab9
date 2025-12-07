0c04c1e4580a76e5aaa114faa666c2508fa567bf
eeemoon <eeemoonart@gmail.com>
2025-12-07 14:54:22 +0200

    Додано автоматичну генерацію Server ID; покращено вивід чату

--- PipeClient.h
+++ PipeClient.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <thread>
 #include <atomic>
+#include <mutex>
 
 class PipeClient : public IClient {
 public:
@@ -15,12 +16,14 @@ public:
     void SendMessage(const std::wstring& message);
 
 private:
-    std::atomic<bool> m_running;
-    HANDLE m_readPipe;
-    HANDLE m_writePipe;
-    std::wstring m_userName;
+std::atomic<bool> m_running;
+HANDLE m_readPipe;
+HANDLE m_writePipe;
+std::wstring m_userName;
+std::wstring m_currentInput;
+std::mutex m_consoleMutex;
 
-    void ListenServer();
-    void SenderLoop();
-    bool ConnectToServer(const std::wstring& basePipeName);
+void ListenServer();
+void ListenConsole();
+bool ConnectToServer(const std::wstring& basePipeName);
 };
