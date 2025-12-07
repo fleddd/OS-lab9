e7e7f3ed167893af8442c8aa214c3cc03a6e36b7
eeemoon <eeemoonart@gmail.com>
2025-12-07 14:06:24 +0200

    Реалізовано сервер та клієнт з Mailslots; покращено вивід чату

--- Config.h
+++ Config.h
@@ -4,6 +4,12 @@ constexpr wchar_t PIPE_BASE_NAME[] = L"\\\\.\\pipe\\MyPipe_";
 constexpr int PIPE_BUFFER_SIZE = 512;
 constexpr int MAX_CLIENTS = 10;
 
+constexpr wchar_t MAILSLOT_BASE_NAME[] = L"\\\\.\\mailslot\\MyMailslot_";
+constexpr int MAILSLOT_BUFFER_SIZE = 512;
+
+constexpr wchar_t INPUT_PROMPT[] = L"> ";
+constexpr int INPUT_PROMPT_LENGTH = sizeof(INPUT_PROMPT) / sizeof(wchar_t) - 1;
+
 struct ClientInfo {
     HANDLE hPipe;
 };
--- MailslotClient.h
+++ MailslotClient.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "IClient.h"
-
+#include <windows.h>
+#undef SendMessage
 #include <string>
-#undef SendMessage;
+#include <thread>
+#include <atomic>
+#include <mutex>
 
 class MailslotClient : public IClient {
 public:
@@ -13,8 +16,17 @@ public:
     void Stop() override;
     void SendMessage(const std::wstring& message) override;
 
-
 private:
-    bool m_running;
+    std::atomic<bool> m_running;
+    HANDLE m_clientMailslot;
+    HANDLE m_serverWriteHandle;
+    std::wstring m_userName;
+    std::wstring m_serverId;
+    std::wstring m_currentInput;
+    std::mutex m_consoleMutex;
+    
     void ListenServer();
+    void ListenConsole();
+    bool ConnectToServer();
+    void ReprintInputLine();
 };
